96dc1a36d89a74e12ef9eec050fe99a3a1783365
AdityaRaj1010 <adityaraj9061811@gmail.com>
2026-05-11 12:50:06 +0530

    execute

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,35 +5,44 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class App {
 
     public static void main(String[] args) {
 
-        WebDriverManager.chromedriver().setup();
+        System.setProperty(
+            "webdriver.chrome.driver",
+            "/usr/local/bin/chromedriver"
+        );
 
         ChromeOptions options = new ChromeOptions();
 
-        options.addArguments("--headless=new");
+        options.setBinary("/usr/bin/google-chrome");
+
+        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
 
         WebDriver driver = new ChromeDriver(options);
 
-        driver.get("https://www.saucedemo.com/");
+try {
 
-        driver.findElement(By.id("user-name"))
-              .sendKeys("standard_user");
+    driver.get("https://www.saucedemo.com/");
 
-        driver.findElement(By.id("password"))
-              .sendKeys("secret_sauce");
+    driver.findElement(By.id("user-name"))
+          .sendKeys("standard_user");
 
-        driver.findElement(By.id("login-button"))
-              .click();
+    driver.findElement(By.id("password"))
+          .sendKeys("secret_sauce");
 
-        System.out.println("Login Successful");
+    driver.findElement(By.id("login-button"))
+          .click();
 
-        driver.quit();
+    System.out.println("Login Successful");
+
+} finally {
+
+    driver.quit();
+}
     }
 }
